920240da4b523b5f83e73733d29043714bd3cc67
Kent Brady <kent_brady@byu.edu>
2021-02-22 20:11:13 -0700

    Fix docker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,50 @@
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    String type = args[0];
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Welcome To the Calculator:");
+    System.out.println("add [int] [int]");
+    System.out.println("subtract [int] [int]");
+    System.out.println("multiply [int] [int]");
+    System.out.println("divide [int] [int]");
+    System.out.println("binary [int]");
+    System.out.println("fibonacci [int]");
+    System.out.println("exit");
+
+    String type = scanner.next();
     Calculator calc = new Calculator();
 
-    if(type.equals("add")) {
-      int left = Integer.parseInt(args[1]);
-      int right = Integer.parseInt(args[2]);
-      int result = calc.add(left, right);
-      System.out.println(result);
-    } else if (type.equals("subtract")) {
-      int left = Integer.parseInt(args[1]);
-      int right = Integer.parseInt(args[2]);
-      int result = calc.subtract(left, right);
-      System.out.println(result);
-    } else if (type.equals("binary")) {
-      int left = Integer.parseInt(args[1]);
-      String result = calc.intToBinaryNumber(left);
-      System.out.println(result);
-    } else if (type.equals("multiply")) {
-      int left = Integer.parseInt(args[1]);
-      int right = Integer.parseInt(args[2]);
-      int result = calc.multiply(left, right);
-      System.out.println(result);
-    } else if (type.equals("divide")) {
-      int left = Integer.parseInt(args[1]);
-      int right = Integer.parseInt(args[2]);
-      int result = calc.divide(left, right);
-      System.out.println(result);
-    } else if (type.equals("fibonacci")) {
-      int left = Integer.parseInt(args[1]);
-      int result = calc.fibonacciNumberFinder(left);
-      System.out.println(result);
+    while (!type.equals("exit")) {
+      int left = Integer.parseInt(scanner.next());
+      if(type.equals("add")) {
+        int right = Integer.parseInt(scanner.next());
+        int result = calc.add(left, right);
+        System.out.println(result);
+      } else if (type.equals("subtract")) {
+        int right = Integer.parseInt(scanner.next());
+        int result = calc.subtract(left, right);
+        System.out.println(result);
+      } else if (type.equals("binary")) {
+        String result = calc.intToBinaryNumber(left);
+        System.out.println(result);
+      } else if (type.equals("multiply")) {
+        int right = Integer.parseInt(scanner.next());
+        int result = calc.multiply(left, right);
+        System.out.println(result);
+      } else if (type.equals("divide")) {
+        int right = Integer.parseInt(scanner.next());
+        int result = calc.divide(left, right);
+        System.out.println(result);
+      } else if (type.equals("fibonacci")) {
+        int result = calc.fibonacciNumberFinder(left);
+        System.out.println(result);
+      }
+
+      type = scanner.next();
     }
 
   }
